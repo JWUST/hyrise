977e1916a321db75a6fa5c6bf949ca7f710ebd25
Martin Grund <grundprinzip@gmail.com>
2013-09-13 12:37:28 +0200

    Moving to pointer for helper

--- src/lib/storage/SequentialHeapMerger.cpp
+++ src/lib/storage/SequentialHeapMerger.cpp
@@ -121,7 +121,7 @@ AbstractTable::SharedDictionaryPtr SequentialHeapMerger::createNewDict(const std
                                                                         bool useValid,
                                                                         const std::vector<bool>& valid) {
   // Heap Queue
-  std::priority_queue<DictMergeHelper<T>> queue;
+  std::priority_queue<DictMergeHelper<T>*> queue;
 
   value_id_mapping.resize(input_tables.size());
 
@@ -131,23 +131,24 @@ AbstractTable::SharedDictionaryPtr SequentialHeapMerger::createNewDict(const std
     auto dict = std::dynamic_pointer_cast<BaseDictionary<T>>(value_id_maps[p]);
     value_id_mapping[p].resize(value_id_maps[p]->size());
 
-    DictMergeHelper<T> helper;
-    helper.it = dict->begin();
-    helper.end = dict->end();
-    helper.tab_index = p;
+    // For each part create a helper that is mapped to the priority queue
+    auto helper = new DictMergeHelper<T>();
+    helper->it = dict->begin();
+    helper->end = dict->end();
+    helper->tab_index = p;
 
     if (useValid) {
-      helper.ref.resize(dict->size(), false);
+      helper->ref.resize(dict->size(), false);
       for(size_t i=0, tabs = input_tables[p]->size(); i < tabs; ++i) {
         if (valid[part_counter + i]) {
-             helper.ref[input_tables[p]->getValueId(column_index, i).valueId] = true;
+             helper->ref[input_tables[p]->getValueId(column_index, i).valueId] = true;
         }
       }
     } else {
-      helper.ref.resize(dict->size(), true);
+      helper->ref.resize(dict->size(), true);
     }
 
-    if (!helper.done()) {
+    if (!helper->done()) {
       queue.push(helper);
     }
 
@@ -163,26 +164,28 @@ AbstractTable::SharedDictionaryPtr SequentialHeapMerger::createNewDict(const std
   auto new_dict = std::make_shared<OrderPreservingDictionary<T>>( hyrise::functional::sum(value_id_mapping, 0ul, [](std::vector<value_id_t>& v){ return v.size(); }));
   while(!queue.empty()) {
 
-    auto element = queue.top();
+    auto& element = queue.top();
 
-    T curr_val = *(element.it);
+    T curr_val = *(element->it);
     queue.pop();
 
     if (first || !assigned || (last_value != curr_val)) {
-      if (element.valid()) {
+      if (element->valid()) {
         new_dict->addValue(curr_val);
       }
     }
 
-    if (element.valid()) {
-      value_id_mapping[element.tab_index][element.it.getValueId()] = new_dict->size() - 1;
+    if (element->valid()) {
+      value_id_mapping[element->tab_index][element->it.getValueId()] = new_dict->size() - 1;
       last_value = curr_val;
       assigned = true;
     }
 
-    element.next();
-    if (!element.done()) {
+    element->next();
+    if (!element->done()) {
       queue.push(element);
+    } else {
+      delete element;
     }
     first = false;
   }
